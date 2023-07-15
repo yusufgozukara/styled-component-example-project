d54f654c9dd07f54ccbc27fc0ceaba71219c3464
Yusuf Gözükara <101390306+yusufgozukara@users.noreply.github.com>
2023-07-15 23:30:45 +0300

    first step

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import Header from './components/Header';
-import { ThemeProvider } from 'styled-components';
-import { GlobalStlyes } from './components/styles/Global';
-import data from './data';
-import Card from './components/Card';
-import Footer from './components/Footer';
-import Container from './components/styles/Container.styled';
+import React from "react";
+import Header from "./components/Header";
 
-const theme = {
-  colors: {
-    header: '#fff',
-    body: '#fff',
-    footer: '#8A1C4A',
-  },
-  responsive: '768px',
-};
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStlyes />
+    <>
       <Header />
-      <Container>
-        {data.map((item, index) => {
-          return <Card {...item} key={index} />;
-        })}
-      </Container>
-      <Footer />
-    </ThemeProvider>
+    </>
   );
 };
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,7 @@
-import StyledCard from './styles/Card.styled';
-import { Image } from './styles/Header.styled';
+import React from "react";
 
-const Card = ({ id, title, body, image }) => {
-  // console.log(image);
-  return (
-    <StyledCard odd={id % 2 && 'row-reverse'}>
-      <div>
-        <h2>{title}</h2>
-        <p>{body}</p>
-      </div>
-      <div>
-        <Image src={`./images/${image}`} />
-      </div>
-    </StyledCard>
-  );
+const Card = () => {
+  return <div>Card</div>;
 };
 
 export default Card;
