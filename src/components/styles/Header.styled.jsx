247816ca7d3d787a3f901bc2b61b369cc8e01944
Yusuf Gözükara <101390306+yusufgozukara@users.noreply.github.com>
2023-07-16 00:56:56 +0300

    commit-1

--- src/components/styles/Header.styled.jsx
+++ src/components/styles/Header.styled.jsx
@@ -7,13 +7,26 @@ const StyledHeader = styled.header`
 
 export const Logo = styled.img`
   width: 300px;
-  padding: 4rem 0;
+  padding: 2rem 0;
+  @media (max-width: ${({ theme }) => theme.responsive}) {
+    text-align: center;
+  }
 `;
 
 export const Nav = styled.nav`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  @media (max-width: ${({ theme }) => theme.responsive}) {
+    flex-direction: column;
+  }
+`;
+
+export const Image = styled.img`
+  width: 350px;
+  @media (max-width: ${({ theme }) => theme.responsive}) {
+    margin: 40px;
+  }
 `;
 
 export default StyledHeader;
